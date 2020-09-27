02effe87c857a55001e1b12480f894e193fd3e9b
kuhufu <huboemail@yeah.net>
2020-09-27 14:37:53 +0800

    update

--- crypto/aes.go
+++ crypto/aes.go
@@ -10,12 +10,13 @@ func PKCS5Padding(cipherText []byte, blockSize int) []byte {
 	padding := blockSize - len(cipherText)%blockSize
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(cipherText, padText...)
+
 }
 
 func PKCS5UnPadding(origData []byte) []byte {
 	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+	unPadding := int(origData[length-1])
+	return origData[:(length - unPadding)]
 }
 
 func AesEncrypt(origData, key []byte) ([]byte, error) {
